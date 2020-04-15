e2047c664fd9c051b610bf11735909d6a348b25a
Moshe B <mosheb.812@gmail.com>
2020-04-15 16:19:25 -0400

    add app folder,check for empty frm

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+import { AppComponent } from './app.component';
+import { BuggetboardComponent } from "./buggetboard/buggetboard.component";
+import { NewBugDiagComponent } from './new-bug-diag/new-bug-diag.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuggetboardComponent,
+    NewBugDiagComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatSnackBarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
