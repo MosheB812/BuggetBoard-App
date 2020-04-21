1966aa92a1df3eff868b4c3086efee867ae47695
Moshe B <mosheb.812@gmail.com>
2020-04-20 21:20:49 -0400

    move to aws api gateway

--- app/src/app/Services/api/api.service.ts
+++ app/src/app/Services/api/api.service.ts
@@ -9,23 +9,23 @@ import { Bug, Group } from "../../Models/models.model";
 
 export class ApiService {
 
-  apiUrl = 'https://my-json-server.typicode.com/MoB812/MyJSONServer';
+  apiUrl = 'https://v5rgkekyhj.execute-api.us-east-1.amazonaws.com/Production/buggetboard';
 
   // Constructor
   constructor(private _httpClient: HttpClient) { }
 
   // Api GET request - Bug List
   getBugList() {
-    return this._httpClient.get<any[]>(this.apiUrl + '/bugs');
+    return this._httpClient.get<any[]>(this.apiUrl + '/bug');
   }
   // Api GET request - Group List
   getGroupList() {
     // Need to implement a way to only get groups for particular uid
     // For now this only provides ALL groups for testing front end
-    return this._httpClient.get<any[]>(this.apiUrl + '/groups');
+    return this._httpClient.get<any[]>(this.apiUrl + '/group');
   }
   // Api POST request - New Bug
   postBug(bug: Bug) {
-    return this._httpClient.post(this.apiUrl + '/bugs', bug);
+    return this._httpClient.post(this.apiUrl + '/bug', bug);
   }
 }
