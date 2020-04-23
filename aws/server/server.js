f02ab20cee88c11ab251030373aad52c265c32e1
Moshe B <mosheb.812@gmail.com>
2020-04-22 21:39:19 -0400

    update bash script, server port

--- aws/server/server.js
+++ aws/server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.static(__dirname + '/dist'));
 
